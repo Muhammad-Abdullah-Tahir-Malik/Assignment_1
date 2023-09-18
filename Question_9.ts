9577756bc43fe5e6558b528536223bd4057a51fc
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-18 15:49:39 +0500

    Q9

---
+++ Question_9.ts
@@ -0,0 +1,5 @@
+/*Favorite Number: Store your favorite number in a variable.Then, using that variable, create a message 
+that reveals your favorite number. Print that message.*/
+var favourite_No : number =2 ;
+var message: string ="My favourite no. is  :  ";
+console.log(message+favourite_No);
