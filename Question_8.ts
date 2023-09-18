39e68cc7bca6d9c3375994f6043536472de2430f
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-18 15:45:45 +0500

    Q8

---
+++ Question_8.ts
@@ -0,0 +1,11 @@
+/*You should create four lines that look like this:
+console.log(5 + 3)
+Your output should simply be four lines with the number 8 appearing once on each line.*/
+
+var a=4;
+console.log((a+a)+"\n");
+console.log((a*2)+"\n");
+console.log((32/a)+"\n");
+console.log((12-a)+"\n");
+
+export{}
