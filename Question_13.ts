4bea9b21e93c159ec31eb324917939fa8d046a1c
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-18 16:10:13 +0500

    Q13

---
+++ Question_13.ts
@@ -0,0 +1,10 @@
+/*Your Own Array: Think of your favorite mode of transportation, such as a motorcycle or a car, and make a list
+that stores several examples. Use your list to print a series of statements about these items, such as
+“I would like to own a Honda motorcycle.*/
+
+let a:string[]=["Bughaty","Supra","Porsh"];
+a.forEach(loop);
+function  loop(value:string){
+console.log(value);
+}
+export{}
