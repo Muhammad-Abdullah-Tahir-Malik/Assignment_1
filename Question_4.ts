e21d26a00315694882dce65728e815e46d4344de
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-18 14:43:23 +0500

    Q4

---
+++ Question_4.ts
@@ -0,0 +1,7 @@
+/*Famous Quote: Find a quote from a famous person you admire. Print the quote and the name of its author.
+Your output should look something like the following, including the quotation marks:*/
+
+var a:string="'Pray to the load for a heart that sees the light of haven ' the eye vision is not same as the vision of the heart'";
+var b: string =" Allama Iqbal ones said,"
+
+console.log(`${b} ${a}`);
