4ef605e5d5843c1e38484a92cfa0757a569843b8
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-18 16:01:47 +0500

    Q11

---
+++ Question_11.ts
@@ -0,0 +1,10 @@
+/*Names: Store the names of a few of your friends in a array called names.Print each person’s name by
+accessing each element in the list, one at a time.*/
+
+var name =["Abdullah","Yaseen","Umer","Ahsan"];
+for(var i=1;i<=4;i++){
+    console.log(name[i]+"\n");
+
+}
+
+export{}
