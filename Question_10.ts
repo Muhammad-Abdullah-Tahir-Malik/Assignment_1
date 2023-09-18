92478772799a4c3a3f4d0cd604aa51c2cb5900fa
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-18 15:55:52 +0500

    Q10

---
+++ Question_10.ts
@@ -0,0 +1,15 @@
+/*Adding Comments: Choose two of the programs you’ve written, and add at least one comment to each. If you don’t 
+have anything specific to write because your programs are too simple at this point, just add your name and 
+the current date at the top of each program file. Then write one sentence describing what the program does.*/
+
+
+// Program Name: Greeting.ts
+// Author: [Abdullah]
+// Date: 2023-09-11
+// Description: This TypeScript program prints a greeting message to the console.
+
+// Greeting message
+const greeting: string = "Hello, World!";
+
+// Print the greeting to the console
+console.log(greeting);
