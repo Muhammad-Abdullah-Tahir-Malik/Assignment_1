d266dfb9d933789c0b709bb5870c4080fe33894a
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-18 14:52:23 +0500

    Q5

---
+++ Question_5.ts
@@ -0,0 +1,8 @@
+/*Famous Quote 2: Repeat Exercise 4, but this time store the famous person’s name in a variable called 
+famous_person.Then compose your message and store it in a new variable called message. Print your message.*/
+
+
+var a:string="'Pray to the load for a heart that sees the light of haven ' the eye vision is not same as the vision of the heart'";
+var famous_person: string =" Allama Iqbal ones said,"
+var message=famous_person+a;
+console.log(message);
