a82f6f235b20a29dc6385031f2b92aa19e34ae4f
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-18 15:00:25 +0500

    Q7

---
+++ Question_7.ts
@@ -0,0 +1,11 @@
+/*Number Eight: Write addition, subtraction, multiplication, and division operationsthat each result 
+in the number 8.Be sure to enclose your operations in print statements to see the results.*/
+
+
+var a=4;
+console.log((a+a)+"\n");
+console.log((a*2)+"\n");
+console.log((32/a)+"\n");
+console.log((12-a)+"\n");
+
+export{}
