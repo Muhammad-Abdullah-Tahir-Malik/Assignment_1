1c423df478545060c1da5fd0c92298894f173b50
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-18 16:05:34 +0500

    Q12

---
+++ Question_12.ts
@@ -0,0 +1,13 @@
+/*Greetings: Start with the array you used in Exercise 11, but instead of just printing each person’s name,
+print a message to them. The text of each message should be the same, but each message should be personalized
+with the person’s name.*/
+
+
+var name =["Abdullah","Yaseen","Umer","Ahsan"];
+var mess="I Love You";
+for(var i=0;i<4;i++){
+    console.log(name[i]+"\t"+mess+"\n");
+
+}
+
+export{}
