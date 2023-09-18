f74be44e58ad26218a9a2ec327d311beded872f3
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-18 14:37:21 +0500

    Q3

---
+++ Question_3.ts
@@ -0,0 +1,12 @@
+/*Name Cases: Store a person’s name in a variable, and then print that person’s 
+name in lowercase, uppercase, and titlecase.*/
+
+let name :string="Abdullah";
+let b=name.toLocaleLowerCase();
+console.log(b);
+b=name.toLocaleUpperCase();
+console.log(b);
+b=name.toLowerCase();
+console.log(b);
+
+export{}
