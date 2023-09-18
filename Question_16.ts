69a5d5cec69837ef769e95472fc07f924bb159cb
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-18 16:35:20 +0500

    Q16

---
+++ Question_16.ts
@@ -0,0 +1,19 @@
+/*More Guests: You just found a bigger dinner table, so now more space is available. Think of three more
+ guests to invite to dinner.
+• Start with your program from Exercise 15. Add a print statement to the end of your program
+informing people that you found a bigger dinner table.
+• Add one new guest to the beginning of your array.
+• Add one new guest to the middle of your array.
+• Use append() to add one new guest to the end of your list.
+• Print a new set of invitation messages, one for each person in your list.*/
+
+
+var guest:string[]=["Ali","Raza","Khalid"]
+guest.forEach(abc);
+function abc(index:string){
+    console.log(`Dear ${index}, .....`);
+}
+guest.splice(1,0,"Tahir","Uumer","Yaseen");
+console.log("\n\n\n")
+guest.forEach(abc);
+
