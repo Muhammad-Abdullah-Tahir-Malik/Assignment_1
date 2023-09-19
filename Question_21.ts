8052251170c865a119524d65b854f846cc9b16c0
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-19 10:33:49 +0500

    Q21

---
+++ Question_21.ts
@@ -0,0 +1,11 @@
+/*They think of something you could store in a TypeScript Object. Write a program that creates Objects
+containing these items.*/
+
+let obj= {
+name: "Ali",
+gender:"Male",
+age: 23
+};
+
+
+console.log(obj);
