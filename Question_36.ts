129552f49ce0bac511f9af6bdb9cfdbcf2ceecb0
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-20 01:39:08 +0500

    Q36

---
+++ Question_36.ts
@@ -0,0 +1,12 @@
+/*T-Shirt: Write a function called make_shirt() that accepts a size and the text of a message that should
+be printed on the shirt. The function should print a sentence summarizing the size of the shirt and the
+message printed on it. Call the function.*/
+
+
+
+function make_Tshirt( size:number,messege:string){
+    console.log( `${size} is the size of your T-Shirt\nThe messege that you ask for print is ${messege}`);
+    }
+    make_Tshirt(32,"Never give up");
+
+    export{}
