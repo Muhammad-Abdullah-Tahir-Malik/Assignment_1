7201b7dd24d4809055b1450c651760a4918fbfc4
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-19 10:49:17 +0500

    Q23

---
+++ Question_23.ts
@@ -0,0 +1,57 @@
+/*Conditional Tests: Write a series of conditional tests. Print a statement describing each test and your
+prediction for the results of each test. Your code should look something like this:
+
+let car = 'subaru';
+
+console.log("Is car == 'subaru'? I predict True.")
+
+console.log(car == 'subaru')
+
+• Look closely at your results, and make sure you understand why each line evaluates to True or False.
+
+• Create at least 10 tests. Have at least 5 tests evaluate to True and another 5 tests evaluate to False.*/
+
+
+
+//              STATEMENT NO.1
+let car = 'subaru';
+console.log("Is car == 'subaru'? I predict True.");
+console.log(car == 'farari');
+//               STATEMENT NO.2
+let bike ='Bugati'
+console.log("bike=='Bugati'");
+console.log(bike=='Bugati');
+//               STATEMENT NO.3
+let a5:number=20;
+var b=(a5==10)? "True":"False";
+console.log(b);
+//               STATEMENT NO.4
+var bk="Honda";
+console.log(bk=="Honda");
+
+//               STATEMENT NO.5
+var b2=120;
+var b1=(b2==120)?"True":"False";
+console.log(b1);
+
+//               STATEMENT NO.6
+var a="Like";
+console.log(a=="Like");
+
+//               STATEMENT NO.7
+var a1="Love";
+console.log(a1=="Love");
+
+//               STATEMENT NO.8
+var a2=250;
+console.log(a2==250);
+
+//               STATEMENT NO.9
+var a3=200;
+console.log(a3==200);
+
+//               STATEMENT NO.10
+var a4=209;
+console.log(a4==209);
+
+export{}
