ab2b099999d3b895d6e66f5480086e467e7e3361
Muhammad Abdullah Tahir <abdullah.tahir.001177@gmail.com>
2023-09-20 02:22:50 +0500

    Q40

---
+++ Question_40.ts
@@ -0,0 +1,23 @@
+/*Album: Write a function called make_album() that builds a Object describing a music album. The function
+should take in an artist name and an album title, and it should return a Object containing these two pieces
+of information. Use the function to make three dictionaries representing different albums. Print each return
+value to show that Objects are storing the album information correctly. Add an optional parameter to
+make_album() that allows you to store the number of tracks on an album. If the calling line includes a value
+for the number of tracks, add that value to the album’s Object. Make at least one new function call that
+includes the number of tracks on an album.*/
+
+
+
+
+function fun(ar:string,abl:string):void{
+    let obj1={
+        art:ar,
+        alb:abl
+    };
+    console.log(`${obj1.alb}   ${obj1.art}`);    
+}
+
+fun("Atif Aslam","Roy");
+fun("Arjit Singh","Ashiki");
+fun("Jubin","Austad Nusrat Fathay Ali Khan");
+export{}
